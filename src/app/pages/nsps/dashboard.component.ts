3a6aa8a6131b232622b33d0ea4c46f1bea36a78f
Paul-Blankson <paul.blankson@amalitech.com>
2025-03-25 04:13:04 +0000

    feat: add nsp dashboard component

---
+++ src/app/pages/nsps/dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "nsps-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"],
+  imports: [],
+})
+export class DashboardComponent {
+  constructor() {}
+}
