e12f969004b518ac31ea0dbc9617666c0da481ce
Jimmy Essel <jimmykodwoessel@gmail.com>
2025-04-29 17:19:33 +0000

    chore: Improved lint to handle other file types

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    'src/**/*.{ts,js,html,scss,json}': [
-      'prettier --write',
-      'eslint --fix'
-    ]
-  };
-  
+  'src/**/*.{ts,js,html,scss,json}': ['prettier --write', 'eslint --fix'],
+  '*.{yml,yaml}': ['prettier --write'], // For .github/workflows/pr-checks.yml
+  Dockerfile: ['hadolint'], // For Dockerfile, if using a linter
+  '*.sh': ['shellcheck'], // For .husky/pre-commit, assuming it’s a shell script
+  '*.js': ['prettier --write', 'eslint --fix'], // For lint-staged.config.js
+};
