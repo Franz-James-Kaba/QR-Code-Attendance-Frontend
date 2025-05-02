92e80baf705e25f728faa110d89f270e1d227200
Paul-Blankson <paul.blankson@amalitech.com>
2025-05-02 15:25:26 +0000

    feat(tailwind): add custom color palette to theme extension

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#065186',
+        secondary: '#E0F0FE',
+        nspBg: '#F0F8FF',
+        nspText: '#292929',
+      },
+    },
   },
   plugins: [],
 }
