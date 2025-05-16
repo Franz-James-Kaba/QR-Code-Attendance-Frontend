65449845212e32a1c9ccabf51391bb0f49e149a6
Paul-Blankson <paul.blankson@amalitech.com>
2025-05-16 14:03:54 +0000

    feat: add event emitter when slide button is clicked

--- src/app/features/NSP/shared/slide-button/slide-button.component.ts
+++ src/app/features/NSP/shared/slide-button/slide-button.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 import { IconComponent } from '../../../../shared/components/icon/icon.component';
 
@@ -10,10 +10,17 @@ import { IconComponent } from '../../../../shared/components/icon/icon.component
   template: `
     <button
       class="w-full bg-primary text-white py-4 px-6 rounded-xl shadow-(#00000026) flex items-center justify-center text-base font-bold gap-2"
+      (click)="startScan()"
     >
       <app-icon viewBox="0 0 21 16" [size]="18" [svgwidth]="21" [svgHeight]="16" path="M15.5 9L19.5 5M19.5 5L15.5 1M19.5 5H6.5C3.73858 5 1.5 7.23858 1.5 10C1.5 12.7614 3.73858 15 6.5 15H11.5" />
       <span>Slide to Check In</span>
     </button>
-  `,
+  `
 })
-export class SlideButtonComponent {}
+export class SlideButtonComponent {
+  @Output() scanRequested = new EventEmitter<void>();
+
+  public startScan(): void {
+    this.scanRequested.emit();
+  }
+}
